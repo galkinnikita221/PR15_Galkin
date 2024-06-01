74a6bf550d1f8f7942c9daa6a226d369609822f8
Ann <iliuvv@mail.ru>
2024-06-01 12:13:08 +0500

    Объявление переменных. Galkin

--- app/src/main/java/com/nik/pr15_galkin/MainActivity.java
+++ app/src/main/java/com/nik/pr15_galkin/MainActivity.java
@@ -2,10 +2,15 @@ package com.nik.pr15_galkin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private MediaRecorder mediaRecorder;
+    private MediaPlayer mediaPlayer;
+    private String fileName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
